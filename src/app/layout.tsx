bf855a3af4922825ea3eaf9a9c78b25399b5c1cd
Issam <Issam.aboulfadl05@gmail.com>
2023-07-06 15:56:43 +0100

    ✨ init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import '@/styles/globals.css'
-
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+	title: "Breadit",
+	description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en" className={cn('')}>
+			<body>{children}</body>
+		</html>
+	);
 }
